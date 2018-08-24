d94d890e48d9165e29ab0e5fe3df9402e1deaf02
Vítor Gonçalves <vitor.goncalves@wit-software.com>
2018-08-24 18:14:21 +0100

    add dependencies

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -40,11 +40,9 @@
 /** IE10 and IE11 requires the following for the Reflect API. */
 // import 'core-js/es6/reflect';
 
-
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
-
 
 /**
  * Web Animations `@angular/platform-browser/animations`
@@ -58,11 +56,11 @@ import 'core-js/es7/reflect';
  * user can disable parts of macroTask/DomEvents patch by setting following flags
  */
 
- // (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
- // (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
- // (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
+// (window as any).__Zone_disable_requestAnimationFrame = true; // disable patch requestAnimationFrame
+// (window as any).__Zone_disable_on_property = true; // disable patch onProperty such as onclick
+// (window as any).__zone_symbol__BLACK_LISTED_EVENTS = ['scroll', 'mousemove']; // disable patch specified eventNames
 
- /*
+/*
  * in IE/Edge developer tools, the addEventListener will also be wrapped by zone.js
  * with the following flag, it will bypass `zone.js` patch for IE/Edge
  */
@@ -71,10 +69,24 @@ import 'core-js/es7/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
-import 'zone.js/dist/zone';  // Included with Angular CLI.
 
-
+import 'zone.js/dist/zone'; // Included with Angular CLI.
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
  */
+
+// import '@webcomponents/custom-elements/src/native-shim';
+
+// Needed on browsers with native `customElements`.
+// (E.g.: Chrome, Opera)
+// ----
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter';
+// import '@webcomponents/custom-elements/src/native-shim';
+// -----
+
+// Needed for browsers without native `customElements`.
+// (E.g.: Edge, Firefox, IE, Safari)
+import '@webcomponents/custom-elements/custom-elements.min';
+
+// import "@webcomponents/webcomponentsjs/bundles/webcomponents-sd-ce";
